39427a469e60b8c678bf8210f4a6a8753622129f
Brice POTÉ <brice.pote@gmail.com>
2021-10-03 23:22:48 +0200

    //WIP : Working on PHP Code Quality

---
+++ src/Entity/UserDataCustomerInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @Corporation : Clic Silver.
+ * @Author: Brice POTE <brice.pote@gmail.com>.
+ * Copyright (c) 2021.
+ * All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace App\Entity;
+
+interface UserDataCustomerInterface
+{
+    public function getAdresse(): ?string;
+    public function setAdresse(?string $_adresse): self;
+}
