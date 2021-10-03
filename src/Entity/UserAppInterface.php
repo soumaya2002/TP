6af375a7cd1a8509f2a98c2fc5b57a56e1f66d61
Brice POTÉ <brice.pote@gmail.com>
2021-10-03 23:42:20 +0200

    Change UserAppInterface for fix PhpStan standards

--- src/Entity/UserAppInterface.php
+++ src/Entity/UserAppInterface.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * @Corporation : Clic Silver.
  * @Author: Brice POTE <brice.pote@gmail.com>.
  * Copyright (c) 2021.
  * All Rights Reserved.
  */
+
 declare(strict_types=1);
 
 namespace App\Entity;
@@ -16,5 +18,4 @@ interface UserAppInterface
 
     public function getLastName(): string;
     public function setLastName(string $_lastName);
-
 }
