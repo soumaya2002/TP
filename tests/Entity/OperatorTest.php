d3a872342a379e76225acd09872c05c0527ecc1e
Brice POTÉ <brice.pote@gmail.com>
2021-10-05 00:13:37 +0200

    Add unit test on Operator

---
+++ tests/Entity/OperatorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Operator;
+use PHPUnit\Framework\TestCase;
+
+class OperatorTest extends TestCase
+{
+    public function testDefaultValues(): void
+    {
+        $operator = new Operator();
+
+        self::assertEmpty($operator->getFirstName());
+        self::assertEmpty($operator->getLastName());
+        self::assertEmpty($operator->getComments());
+    }
+
+    public function testSettersAndGetters(): void
+    {
+        $operator = new Operator();
+
+        self::assertInstanceOf(Operator::class, $operator->setFirstsName('Brice'));
+        self::assertInstanceOf(Operator::class, $operator->setLastName('POTE'));
+        self::assertInstanceOf(Operator::class, $operator->setComment('Ceci est un commentaire'));
+    }
+}
