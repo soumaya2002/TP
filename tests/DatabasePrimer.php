a460f3909b0fbe710890c83a3d80227e06f4a514
Brice POTÉ <brice.pote@gmail.com>
2021-10-15 20:27:02 +0200

    //WIP : debugging CI/CD issue

--- tests/DatabasePrimer.php
+++ tests/DatabasePrimer.php
@@ -13,25 +13,28 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
-use Doctrine\ORM\Tools\SchemaTool;
-use LogicException;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 final class DatabasePrimer
 {
-    public static function prime(KernelInterface $kernel)
+//    public static function prime(KernelInterface $kernel)
+//    {
+//        // Make sure we are in the test environment
+//        if ('test' !== $kernel->getEnvironment()) {
+//            throw new LogicException('Primer must be executed in the test environment');
+//        }
+//
+//        // Get the entity manager from the service container
+//        $entityManager = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+//
+//        // Run the schema update tool using our entity metadata
+//        $metadata = $entityManager->getMetadataFactory()->getAllMetadata();
+//        $schemaTool = new SchemaTool($entityManager);
+//        $schemaTool->updateSchema($metadata);
+//    }
+
+    public static function prime(KernelInterface $kernel): bool
     {
-        // Make sure we are in the test environment
-        if ('test' !== $kernel->getEnvironment()) {
-            throw new LogicException('Primer must be executed in the test environment');
-        }
-
-        // Get the entity manager from the service container
-        $entityManager = $kernel->getContainer()->get('doctrine.orm.entity_manager');
-
-        // Run the schema update tool using our entity metadata
-        $metadata = $entityManager->getMetadataFactory()->getAllMetadata();
-        $schemaTool = new SchemaTool($entityManager);
-        $schemaTool->updateSchema($metadata);
+        return true;
     }
 }
